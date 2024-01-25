05a70f9bedc01e19c267ee40bb647f015f00f739
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-01-25 15:19:45 +0800

    changes files

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,27 @@
-// vite.config.js
-import {resolve} from 'path';
-import {defineConfig} from 'vite';
-import {dependencies, devDependencies} from './package.json';
+import { resolve } from 'path'
 
+import { defineConfig } from 'vite'
+
+import { dependencies, devDependencies } from './package.json'
+// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'index',
       // the proper extensions will be added
-      fileName: 'my-lib',
+      fileName: 'index',
+      formats: ['es'],
+      name: 'live-bundlers',
     },
     rollupOptions: {
-      external: ['react/jsx-runtime', ...Object.keys(devDependencies), ...Object.keys(dependencies)],
-      output: {
-        dir: 'dist',
-        entryFileNames: '[name].js',
-        format: 'es'
-      },
+      external: [
+        'react/jsx-runtime',
+        ...Object.keys(dependencies),
+        ...Object.keys(devDependencies),
+      ],
     },
+    sourcemap: true,
     target: 'esnext',
   },
-});
+})
